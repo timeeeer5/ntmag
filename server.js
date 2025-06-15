218bc86deb811c75cbc21ddf5951c299e9b1b5b2
timeeeer5 <dmsguq3721@gmail.com>
2025-06-15 11:29:02 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const { exec } = require('child_process');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/generate', (req, res) => {
+  const { text, filename } = req.body;
+  const safeFilename = filename || 'output.mp4';
+
+  const command = `ffmpeg -f lavfi -i color=c=black:s=720x1280:d=5 -vf "drawtext=text='${text}':fontcolor=white:fontsize=50:x=(w-text_w)/2:y=(h-text_h)/2" -y ${safeFilename}`;
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      return res.status(500).send({ error: stderr });
+    }
+    res.send({
+      message: 'Video created',
+      file: safeFilename
+    });
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('FFmpeg API is working!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
